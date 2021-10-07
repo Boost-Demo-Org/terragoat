c89783abbc6eec3732bf862e945d08eb7a71b810
David Scrobonia <dscrobonia@users.noreply.github.com>
2021-10-07 13:07:41 -0700

    Update athena.tf

--- athena.tf
+++ athena.tf
@@ -1,6 +1,9 @@
 resource "aws_athena_database" "athena_database_one" {
     bucket = "some-bucket"
     name = "athena db one"
+    encryption_configuration {
+        encryption_option = "SSE_S3"
+    }
 }
 
 resource "aws_athena_database" "athena_database_two" {
