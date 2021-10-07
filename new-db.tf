6687432425214ae46f7e679770306a4f2f59e18e
David Scrobonia <dscrobonia@users.noreply.github.com>
2021-10-07 00:59:56 -0700

    Update new-db.tf

--- new-db.tf
+++ new-db.tf
@@ -5,3 +5,8 @@ resource "aws_athena_database" "athena_database" {
         encryption_option = "SSE_S3"
     }
 }
+
+resource "aws_athena_database" "athena_database_two" {
+    bucket = "some-bucket"
+    name = "athena db two"
+}
